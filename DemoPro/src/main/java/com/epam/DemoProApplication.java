6cf250d06ef38d963499b98cf607318181e24174
supriya-divvela <81727984+supriya-divvela@users.noreply.github.com>
2023-08-06 19:43:51 +0530

    Add files via upload

---
+++ DemoPro/src/main/java/com/epam/DemoProApplication.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProApplication.class, args);
+	}
+
+}
